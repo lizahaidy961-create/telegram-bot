f12d019e4d77fffbb766bc9862e03b6cc0209c2e
Vini <lizahaidy961@gmail.com>
2026-03-03 10:08:21 -0300

    bot modo teste

--- bot.py
+++ bot.py
@@ -393,8 +393,3 @@ def cron_check_subscriptions():
 @app.route("/")
 def home():
     return "Bot online", 200
-
-# ---------- RUN FLASK ----------
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
